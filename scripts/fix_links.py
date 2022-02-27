8598f38e83ecd0a4a1f91d97a53ab5b875f56c53
Jesse Hoogland <jessequinten@gmail.com>
2022-02-27 08:28:15 -0300

    FIX fix links script to truncate end of file

--- scripts/fix_links.py
+++ scripts/fix_links.py
@@ -5,39 +5,41 @@ from scripts.md import Markdown
 def fix_links(path="../Concepts"):
     md = Markdown()
 
-    for filepath in os.listdir(path):
+    for (i, filepath) in enumerate(os.listdir(path)):
         with open(os.path.join(path, filepath), "r+") as f:
             #  Read the entire file at a time (these are relatively small text files)
             data = f.read()
 
-            # Fix the tags
-            tag_matches = re.findall("\[(.?)\]\((http.*/tag/(.?))\)", data)
+            # Check to make sure all referenced tags exist in our Concepts folder
+            tag_matches = re.findall("\[(.*?)\]\((http.*/tag/(.*?))\)", data)
             for (tag, link, slug) in tag_matches:
                 tag = md.clean_link(tag)
                 if not os.path.isfile(os.path.join(path, tag + ".md")):
-                    # Make sure the referenced tag exists in our Concepts folder
                     print(f"[Error] {tag}.md not found")
 
             # Replace the external md-style links with local wiki-style links
-            data = re.sub("\[(.?)\]\((http.*/tag/(.*))\)", r'[[\1]]', data)
-            print(filepath, data, sep="\n")
+            data = re.sub("\[(.*?)\]\((http.*/tag/(.*?))\)", r'[[\1]]', data)
+
+            print(filepath, tag_matches, sep=": ")
+            print(data)
+            print("-" * 120)
 
             # Fix links to posts
-            post_matches = re.findall("\[(.?)\]\((http.*/post/(.?))\)", data)
+            post_matches = re.findall("\[(.*?)\]\((http.*/post/(.*?))\)", data)
             for (post, link, slug) in tag_matches:
                 post = md.clean_link(post)
                 if not os.path.isfile(os.path.join(path, post + ".md")):
                     # Make sure the referenced tag exists in our Concepts folder
                     print(f"[Error] {post}.md not found")
 
-                    return
 
-
-            # Write to the file
-            f.seek(0)  # To overwrite (rather than append)
+            # Write to the file (overwrite rather than append)
+            f.seek(0)
             f.write(data)
+            f.truncate()
 
-            return
+            if i > 0:
+                return
 
 
 if __name__ == "__main__":
