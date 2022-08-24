f303ea7322fc759b48fdae81370a1978e9ba0ed2
Jesse Hoogland <jessequinten@gmail.com>
2022-08-24 22:45:58 +0200

    Even more jargon

--- scripts/src/lw/jargon.ts
+++ scripts/src/lw/jargon.ts
@@ -1,5 +1,7 @@
 /**
- * Compiled from the [CFAR Jargon Appendix](https://www.lesswrong.com/posts/fbv9FWss6ScDMJiAx/appendix-jargon-dictionary).
+ * Compiled from:
+ * - [CFAR Jargon Appendix](https://www.lesswrong.com/posts/fbv9FWss6ScDMJiAx/appendix-jargon-dictionary).
+ * - [LW "Jargon" tag](https://www.lesswrong.com/tag/lesswrong-jargon)
  */
 
 import fs from "fs";
@@ -18,7 +20,7 @@ interface Post {
     }
 }
 
-export const loadJargon = async () => {
+const loadCAFRJargon = async () => {
     const query = gql`{
         post(input: {
             selector:{
@@ -46,19 +48,60 @@ export const loadJargon = async () => {
     return request('https://www.lesswrong.com/graphql', query)
         .then(({ post }: { post: { result: Post } }) => {
             return splitSections(post.result.contents.markdown);
-        }).then(sections => {
-            fs.writeFileSync('./data/jargon.json', JSON.stringify(sections, null, 2))
-            return sections;
         })
 }
+
+const loadLWJargon = async () => {
+    const query = gql`{
+        tag(input: {
+          selector:{
+            slug: "lesswrong-jargon"
+          }
+        }) {
+            result {
+                _id
+                  name
+                  description {
+                  markdown
+                }
+            }
+        }
+    }`
+
+    return request('https://www.lesswrong.com/graphql', query)
+        .then(({ tag }: { tag: { result: { _id: string, name: string, description: { markdown: string } } } }) => {
+            return splitSections(tag.result.description.markdown);
+        }).then(sections => sections
+            .filter(({ body }) => body !== "\n-----")
+            .map((item) => {
+                if (item.title.endsWith(":")) {
+                    item.title = item.title.slice(0, -1);
+                } else if (item.body.startsWith(":")) {
+                    item.body = item.body.slice(2);
+                }
+                return item
+            })
+        )
+}
+
 
 export const splitSections = async (markdown: string) => {
     // Unfortunately, some posts use `\n\n**Section Heading**\n\n` to mark headings (instead of `#`s)
     return ("\n\n" + markdown).split("\n\n**")
-        .map(section => section.split("**\n\n"))
+        .map(section => section.split("**"))
         .filter((items) => items.length === 2 && items[0])
         .map(([title, body]) => ({ title, body }))    
+}
+
+export const loadJargon = async () => {
+    const jargon = [
+        ...(await loadCAFRJargon()),
+        ...(await loadLWJargon()),
+    ];
+
+    fs.writeFileSync('./data/jargon.json', JSON.stringify(jargon, null, 2))
+    return jargon;
 
 }
 
-// (await loadJargon().then(jargon => console.log(JSON.stringify(jargon, null, 2))))
+(await loadJargon().then(jargon => console.log(JSON.stringify(jargon, null, 2))))
